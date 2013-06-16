37e9b7def64ee2e708ab72a5493ade61213091e5
David Vitrant <david@synteny.us>
2013-06-16 14:15:10 -0400

    first commit of glados

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glados::Application.config.secret_token = 'e634abe83588efe9157e943a33a0069b82671aae214b77bb47ad5f579a78b3b57b84ada500c326d6c54f74b594207cd7ddb1c1203d8c57eb91d18af482c37155'
