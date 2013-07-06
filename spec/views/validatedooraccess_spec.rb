c859f6d611f23fcd7f3ca69967ba7b1d7e81edee
prussiap <david@synteny.us>
2013-07-06 15:40:47 -0400

    removed view test as told we don't usually test for that

--- spec/views/validatedooraccess_spec.rb
+++ spec/views/validatedooraccess_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "validatedooraccess" do
-  let!(:door) { FactoryGirl.create :door }
-  let!(:fob) { FactoryGirl.create :fob }
-    before { visit dooraccess_path }
-
-  it "should have door field" do
-    select door.doorname :from => "door[door_id]"
-  end
-end
+#describe "validatedooraccess" do
+#  let!(:door) { FactoryGirl.create :door }
+#  let!(:fob) { FactoryGirl.create :fob }
+# before { visit dooraccess_path }
+#
+#  it "should have door field" do
+#    select door.doorname :from => "door[door_id]"
+#  end
+#end
